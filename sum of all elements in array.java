98a69695e1dfcff82efd3347ad7f3a2ee9d961da
Vijaysoni2004 <vijaysoni0174917@gmail.com>
2024-09-17 11:01:06 +0530

    Create sum of all elements in array.java

---
+++ sum of all elements in array.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String []args) {
+      int arr[] = {4,6,1,9,2};
+    int sum=0;
+    int n = arr.length;
+    for(int i=0;i<n;i++) {
+        
+           sum+=arr[i]; 
+        
+        
+    }
+    System.out.println(sum);
+    
+    }
+}
